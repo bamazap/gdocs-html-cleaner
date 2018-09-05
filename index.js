461955feefe8384bb2abb773192c2121455a93f0
Barry Anthony McNamara III <bamazap@hotmail.com>
2018-09-04 21:58:12 -0400

    File reading. ESlint.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { ArgumentParser } = require("argparse");
+const fs = require('fs');
+const path = require('path');
+const { ArgumentParser } = require('argparse');
 
 const parser = new ArgumentParser({
   version: '1.0.0',
@@ -19,3 +21,6 @@ parser.addArgument(
 );
 
 const args = parser.parseArgs();
+
+const infilePath = path.resolve(args.infile);
+const dirtyHTML = fs.readFileSync(infilePath, { encoding: 'utf8' });
